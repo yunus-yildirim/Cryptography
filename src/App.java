09f9b7630261d86affb42d13a2f74bfa0c1b0b46
Yunus Yıldırım <ynsyldrm6@gmail.com>
2022-05-11 00:43:11 +0300

    Generation CBC to 128 & *.txt files

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyPair;
@@ -8,13 +12,17 @@ import java.security.MessageDigest;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.security.spec.AlgorithmParameterSpec;
 import java.util.Base64;
 import java.util.Scanner;
 
 import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
 import javax.crypto.KeyGenerator;
 import javax.crypto.Mac;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 
 public class App {
     static byte[] buf = new byte[1024];
@@ -24,20 +32,14 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
-        
-
-        // Create SecureRandom
-        SecureRandom securerandom = new SecureRandom();
-        KeyGenerator keygenerator = KeyGenerator.getInstance("AES");
-
         // Keys.txt file create and open for write
         String filePath = "keys.txt";
         FileWriter fileWriter = new FileWriter(filePath, false);
         PrintWriter printWriter = new PrintWriter(fileWriter);
 
-        /******Q1************/
+        /****** Q1 ************/
         printWriter.println("// Question 1");
-        
+
         // Create key pair generator with RSA
         KeyPairGenerator kpgRSA = KeyPairGenerator.getInstance("RSA");
         // The length of the keys should be at least 1024 bits
@@ -53,12 +55,14 @@ public class App {
         // The length of the keys 256 bits
         kpgEC.initialize(256);
 
-        // Generation an Elliptic-Curve Diffie Helman public-private key pairs. Kb(+) and Kb(-)
+        // Generation an Elliptic-Curve Diffie Helman public-private key pairs. Kb(+)
+        // and Kb(-)
         KeyPair kpEC = kpgEC.generateKeyPair();
         PublicKey publicKb = kpEC.getPublic();
         PrivateKey privateKb = kpEC.getPrivate();
 
-        // Generation an Elliptic-Curve Diffie Helman public-private key pairs. Kc(+) and Kc(-)
+        // Generation an Elliptic-Curve Diffie Helman public-private key pairs. Kc(+)
+        // and Kc(-)
         KeyPair kpEC2 = kpgEC.generateKeyPair();
         PublicKey publicKc = kpEC2.getPublic();
         PrivateKey privateKc = kpEC2.getPrivate();
@@ -71,14 +75,18 @@ public class App {
         printWriter.println("Public Key Kc(+)\n" + enc.encodeToString(publicKc.getEncoded()));
         printWriter.println("Private Key Kc(-)\n" + enc.encodeToString(privateKc.getEncoded()));
 
-        /******Q2************/
+        /****** Q2 ************/
         printWriter.println("\n// Question 2");
+
+        // Create SecureRandom
+        SecureRandom securerandom = new SecureRandom();
+        KeyGenerator keygenerator = KeyGenerator.getInstance("AES");
 
         // Generation Symetric keys
         keygenerator.init(128, securerandom);
         SecretKey K1 = keygenerator.generateKey();
         printWriter.println("K1: " + new String(enc.encodeToString(K1.getEncoded())));
-        
+
         keygenerator.init(256, securerandom);
         SecretKey K2 = keygenerator.generateKey();
         printWriter.println("K2: " + new String(enc.encodeToString(K2.getEncoded())));
@@ -102,8 +110,8 @@ public class App {
         byte[] decryptedK2 = cipher.doFinal(encryptedK2);
         printWriter.println("Decryption K2 with Ka(-):" + new String(decryptedK2));
 
-        /******Q3************/
-        printWriter.println("// Question 3");
+        /****** Q3 ************/
+        printWriter.println("\n// Question 3");
 
         // Get message from file
         String message = "";
@@ -125,26 +133,81 @@ public class App {
         }
         printWriter.println("H(m): " + sb.toString());
 
-        // Encryption with Ka(-) 
+        // Encryption with Ka(-)
         cipher.init(Cipher.ENCRYPT_MODE, privateKa);
         byte[] encrypt_Hm = cipher.doFinal(message.getBytes());
-        printWriter.println("Ka(-)(H(m)): " + new String(enc.encodeToString(encrypt_Hm)));
+        printWriter.println("Digital Signature: " + new String(enc.encodeToString(encrypt_Hm)));
 
         // Decryption with Ka(+)
         cipher.init(Cipher.DECRYPT_MODE, publicKa);
         byte[] decHm = cipher.doFinal(encrypt_Hm);
         printWriter.println("Message: " + new String(decHm));
 
+        /****** Q4 ************/
+        printWriter.println("\n// Question 4");
 
+        byte[] iv = new byte[16];
+        AlgorithmParameterSpec paramSpec = new IvParameterSpec(iv);
 
-        /******Q5************/
-        printWriter.println("// Question 5");
+        try {
+            ecipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            dcipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            // Encrypt
+            long startTime3 = System.nanoTime();
+            ecipher.init(Cipher.ENCRYPT_MODE, K1, paramSpec);
+            long endTime3 = System.nanoTime();
+            encryptAES(new FileInputStream("q4.txt"), new FileOutputStream("q4_enc_k1.txt"));
+            long time3 = endTime3 - startTime3;
+            printWriter.println("AES128 encryption took " + time3 + " nanosecond");
 
-        // Generate a message authentication code (HMAC-SHA256) using K1 the symmetric keys.
+            // Decrypt
+            long startTime4 = System.nanoTime();
+            dcipher.init(Cipher.DECRYPT_MODE, K1, paramSpec);
+            long endTime4 = System.nanoTime();
+            decryptAES(new FileInputStream("q4_enc_k1.txt"), new FileOutputStream("q4_dec_k1.txt"));
+            long time4 = endTime4 - startTime4;
+            printWriter.println("AES128 decryption took "+time4 +" nanosecond");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        /****** Q5 ************/
+        printWriter.println("\n// Question 5");
+
+        // Generate a message authentication code (HMAC-SHA256) using K1 the symmetric
+        // keys.
         Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
-  	    sha256_HMAC.init(K1);
+        sha256_HMAC.init(K1);
 
         printWriter.close();
 
+    }
+
+    public static void encryptAES(InputStream in, OutputStream out){
+        try{
+            out = new CipherOutputStream(out, ecipher);
+            int numRead = 0;
+            while ((numRead = in.read(buf)) >= 0){
+                out.write(buf, 0, numRead);
+            }
+            out.close();
+        }
+        catch (java.io.IOException e){
+        }
+    }
+    
+    public static void decryptAES(InputStream in, OutputStream out){
+        try{
+            in = new CipherInputStream(in, dcipher);
+            int numRead = 0;
+            while ((numRead = in.read(buf)) >= 0){
+                out.write(buf, 0, numRead);
+            }
+            out.close();
+        }
+        catch (java.io.IOException e)
+        {
+        }
     }
 }

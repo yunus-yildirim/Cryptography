427fde4294e3cf967f9f1d685f39d88f2d6877de
Yunus Yıldırım <ynsyldrm6@gmail.com>
2022-05-10 14:10:27 +0300

    Write keys in .txt

--- src/App.java
+++ src/App.java
@@ -1,43 +1,36 @@
-import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.security.*;
+import java.io.PrintWriter;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.Base64;
 
 public class App {
-	static byte[] buf = new byte[1024];
-	static Base64.Encoder enc = Base64.getEncoder();
-    
-    public static void main(String[] args) throws Exception {
-        
-        /// Question 1       	
-        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");     
-        kpg.initialize(1024);   
-        
-        KeyPair kp = kpg.generateKeyPair();   
-        PrivateKey prk = kp.getPrivate();   
-        PublicKey puk = kp.getPublic();
-        
-        try {
-            File myObj = new File("Keys.txt");
-            if (myObj.createNewFile()) {
-              System.out.println("File created: " + myObj.getName());
-            } else {
-              System.out.println("File already exists.");
-            }
-          } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-          }
+    static byte[] buf = new byte[1024];
+    static Base64.Encoder enc = Base64.getEncoder();
 
-          try {
-            FileWriter myWriter = new FileWriter("Keys.txt");
-            myWriter.write("Public Key KA(+): \n" + enc.encodeToString(puk.getEncoded()) + "\n" 
-            + "Private Key KA(-): \n" + enc.encodeToString(prk.getEncoded()) + "\n\n");
-            myWriter.close();
-          } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-          }
+    public static void main(String[] args) throws Exception {
+
+        String filePath = "Keys.txt";
+        PrintWriter writer = new PrintWriter(filePath);
+        writer.print("");
+        writer.close();
+
+        /// Question 1
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(1024);
+
+        KeyPair kp = kpg.generateKeyPair();
+        PrivateKey prk = kp.getPrivate();
+        PublicKey puk = kp.getPublic();
+
+        try (FileWriter fileWriter = new FileWriter(filePath, true);
+                PrintWriter printWriter = new PrintWriter(fileWriter);) {
+
+            printWriter.println("Public Key KA(+): \n" + enc.encodeToString(puk.getEncoded()) + "\n");
+            printWriter.println("Private Key KA(-): \n" + enc.encodeToString(prk.getEncoded()) + "\n");
+        }
+
     }
 }

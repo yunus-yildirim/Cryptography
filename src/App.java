3f2ca2a6e0e332130cd0c8aba5df54862f915e5b
Yunus Yıldırım <ynsyldrm6@gmail.com>
2022-05-10 16:23:40 +0300

    Remove of symetric keys methods

--- src/App.java
+++ src/App.java
@@ -24,12 +24,19 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
+        // Create RSA cipher
+        Cipher cipher = Cipher.getInstance("RSA");
+
+        // Create SecureRandom
+        SecureRandom securerandom = new SecureRandom();
+        KeyGenerator keygenerator = KeyGenerator.getInstance(AES);
+
         String filePath = "keys.txt";
         FileWriter fileWriter = new FileWriter(filePath, false);
         PrintWriter printWriter = new PrintWriter(fileWriter);
 
-        // Question 1 - a
-        printWriter.println("// Question 1 - a\n");
+        /******Q1************/
+        printWriter.println("// Question 1");
 
         KeyPairGenerator kpgRSAGenerator = KeyPairGenerator.getInstance("RSA");
         kpgRSAGenerator.initialize(1024);
@@ -40,9 +47,6 @@ public class App {
 
         printWriter.println("Public Key Ka(+): \n" + enc.encodeToString(publicKa.getEncoded()) + "\n");
         printWriter.println("Private Key Ka(-): \n" + enc.encodeToString(privateKa.getEncoded()) + "\n\n");
-
-        // Question 1 - b
-        printWriter.println("// Question 1 - b\n");
 
         KeyPairGenerator kpgECGenerator = KeyPairGenerator.getInstance("EC");
         kpgECGenerator.initialize(256);
@@ -58,8 +62,40 @@ public class App {
         printWriter.println("Private Key Kb(-): \n" + enc.encodeToString(privateKb.getEncoded()) + "\n");
         printWriter.println("Public Key Kc(+): \n" + enc.encodeToString(publicKc.getEncoded()) + "\n");
         printWriter.println("Private Key Kc(-): \n" + enc.encodeToString(privateKc.getEncoded()) + "\n\n");
+
+        /******Q2************/
+        printWriter.println("// Question 2");
+
+        // Generation Symetric keys
+        keygenerator.init(128, securerandom);
+        SecretKey symKey128 = keygenerator.generateKey();
+        printWriter.println("K1: " + new String(enc.encodeToString(symKey128.getEncoded())));
         
-        // Question 3
+        keygenerator.init(256, securerandom);
+        SecretKey symKey256 = keygenerator.generateKey();
+        printWriter.println("K2: " + new String(enc.encodeToString(symKey256.getEncoded())));
+
+        // K1 encryption and decryption
+        printWriter.println("\nEncryption K1 with Ka(+):");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKa);
+        byte[] encrypted128 = cipher.doFinal((enc.encodeToString(symKey128.getEncoded())).getBytes());
+        printWriter.println(new String(enc.encodeToString(encrypted128)));
+        printWriter.println("\nDecryption K1 with with Ka(-):");
+        cipher.init(Cipher.DECRYPT_MODE, privateKa);
+        byte[] decrypted128 = cipher.doFinal(encrypted128);
+        printWriter.println(new String(decrypted128)); // This is a secret message
+
+        // K2 encryption and decryption
+        printWriter.println("\n\nEncryption K2 with public key:\n");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKa);
+        byte[] encrypted256 = cipher.doFinal((enc.encodeToString(symKey256.getEncoded())).getBytes());
+        printWriter.println(new String(enc.encodeToString(encrypted256)));
+        printWriter.println("\nDecryption K2 with private key:\n");
+        cipher.init(Cipher.DECRYPT_MODE, privateKa);
+        byte[] decrypted256 = cipher.doFinal(encrypted256);
+        printWriter.println(new String(decrypted256));
+
+        /******Q3************/
         printWriter.println("// Question 3");
 
         // Get message from file
@@ -80,70 +116,19 @@ public class App {
         for (byte b : hashInBytes) {
             sb.append(String.format("%02x", b));
         }
-        printWriter.println("\nH(m): " + sb.toString());
+        printWriter.println("H(m): " + sb.toString());
 
-        //Create RSA cipher
-        Cipher cipher = Cipher.getInstance("RSA");
         // Encryption with Ka(-)
         cipher.init(Cipher.ENCRYPT_MODE, privateKa);
-        byte [] encrypt_Hm = cipher.doFinal(message.getBytes());
-        printWriter.println("\nKa(-)(H(m)): "+new String(enc.encodeToString(encrypt_Hm)));
+        byte[] encrypt_Hm = cipher.doFinal(message.getBytes());
+        printWriter.println("Ka(-)(H(m)): " + new String(enc.encodeToString(encrypt_Hm)));
 
         // Decryption with Ka(+)
         cipher.init(Cipher.DECRYPT_MODE, publicKa);
-        byte[] decHm = cipher.doFinal(encrypt_Hm);                                 
-        printWriter.println("\nMessage: " +new String(decHm));  
+        byte[] decHm = cipher.doFinal(encrypt_Hm);
+        printWriter.println("Message: " + new String(decHm));
 
-
-        
         printWriter.close();
 
-        // Question 2
-
-        SecretKey symKey128 = generateAESKey(128);
-        System.out.println("K1: " + new String(enc.encodeToString(symKey128.getEncoded())) + "\n");
-        SecretKey symKey256 = generateAESKey(256);
-        System.out.println("K2: " + new String(enc.encodeToString(symKey256.getEncoded())) + "\n");
-
-        System.out.println("Encryption K1 with public key:\n");
-        byte[] encrypted128 = encrypt(publicKa, new String(enc.encodeToString(symKey128.getEncoded())));
-        System.out.println(new String(enc.encodeToString(encrypted128)));
-        System.out.println("\nDecryption K1 with private key:\n");
-        byte[] decrypted128 = decrypt(privateKa, encrypted128);
-        System.out.println(new String(decrypted128)); // This is a secret message
-
-        System.out.println("\n\nEncryption K2 with public key:\n");
-        byte[] encrypted256 = encrypt(publicKa, new String(enc.encodeToString(symKey256.getEncoded())));
-        System.out.println(new String(enc.encodeToString(encrypted256)));
-        System.out.println("\nDecryption K2 with private key:\n");
-        byte[] decrypted256 = decrypt(privateKa, encrypted256);
-        System.out.println(new String(decrypted256));
-
     }
-
-    public static SecretKey generateAESKey(int bit)
-            throws Exception {
-        SecureRandom securerandom = new SecureRandom();
-        KeyGenerator keygenerator = KeyGenerator.getInstance(AES);
-
-        keygenerator.init(bit, securerandom);
-        SecretKey key = keygenerator.generateKey();
-
-        return key;
-    }
-
-    // Question 2
-
-    public static byte[] decrypt(PrivateKey privateKey, byte[] encrypted) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.DECRYPT_MODE, privateKey);
-        return cipher.doFinal(encrypted);
-    }
-
-    public static byte[] encrypt(PublicKey publicKey, String message) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        return cipher.doFinal(message.getBytes());
-    }
-
 }
